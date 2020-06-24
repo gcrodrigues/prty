617634a29ab710019ce972a5ebdc9e6301a878b6
Gustavo <gcrodrigues413@gmail.com>
2020-06-24 13:06:12 -0300

    Componente AppContainer

---
+++ src/components/AppContainer/AppContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./AppContainer.module.css";
+
+function AppContainer({ children }) {
+  return <div className={styles.container}>{children}</div>;
+}
+
+export default AppContainer;
