5c2442a8a1a82ab5ea3b7477b1b383e9f5c3f88b
Gustavo <gcrodrigues413@gmail.com>
2020-06-24 13:07:39 -0300

    Arquivo index da /components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as SignForm } from "./SignForm/SignForm";
+export { default as Drawer } from "./Drawer/Drawer";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as SignButtons } from "./SignButtons/SignButtons";
+export { default as EventList } from "./EventList/EventList";
+export { default as EventCard } from "./EventCard/EventCard";
+export { default as AppContainer } from "./AppContainer/AppContainer";
