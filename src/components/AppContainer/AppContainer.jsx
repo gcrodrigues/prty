b97fd80996cd153c5efa2c33eaca183e8cd14ce5
Gustavo <gcrodrigues413@gmail.com>
2020-06-24 13:54:46 -0300

    adicionando Drawer e Navbar ao componente

--- src/components/AppContainer/AppContainer.jsx
+++ src/components/AppContainer/AppContainer.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Navbar, Drawer } from "../";
 
 import styles from "./AppContainer.module.css";
 
 function AppContainer({ children }) {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={styles.container}>
+      <Drawer />
+      <Navbar />
+      {children}
+    </div>
+  );
 }
 
 export default AppContainer;
