cce3e5d8df89b97417b21db42dc5b87c266d8090
Gustavo <gcrodrigues413@gmail.com>
2020-06-23 21:03:08 -0300

    First commit

---
+++ src/pages/Events/Events.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./Events.module.css";
+
+const Events = () => {
+  return (
+    <div>
+      <h1>Events</h1>
+    </div>
+  );
+};
+
+export default Events;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home/Home";
+export { default as Signup } from "./Signup/Signup";
+export { default as Login } from "./Login/Login";
+export { default as Dashboard } from "./Dashboard/Dashboard";
+export { default as Events } from "./Events/Events";
+export { default as Settings } from "./Settings/Settings";
