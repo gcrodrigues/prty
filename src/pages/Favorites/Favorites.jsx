19faa0d398323bff99184a2ed7322dc060975e91
Gustavo <gcrodrigues413@gmail.com>
2020-06-29 17:26:21 -0300

    Página de eventos favoritados

---
+++ src/pages/Favorites/Favorites.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AppContainer } from "../../components";
+
+import styles from "./Favorites.module.css";
+
+function Favorites() {
+  return (
+    <AppContainer>
+      <main className={styles.main}></main>
+    </AppContainer>
+  );
+}
+
+export default Favorites;
