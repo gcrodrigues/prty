bd434d8992cf6be053b7824e1f26f6e334be8927
Gustavo <gcrodrigues413@gmail.com>
2020-06-29 00:11:10 -0300

    Página de busca de eventos

---
+++ src/pages/Search/Search.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AppContainer } from "../../components";
+
+import styles from "./Search.module.css";
+
+function Search() {
+  return (
+    <AppContainer>
+      <main className={styles.main}></main>
+    </AppContainer>
+  );
+}
+
+export default Search;
