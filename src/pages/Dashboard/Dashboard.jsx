ed47a46b64a7e6581d4aa5a4a78da499e218d102
Gustavo <gcrodrigues413@gmail.com>
2020-06-29 00:14:11 -0300

    Página de criação de um evento

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 
+import { AppContainer } from "../../components";
 import styles from "./Dashboard.module.css";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <AppContainer>
+      <main className={styles.main}>
+        <h1>Cadastre seu evento</h1>
+        <form className={styles.form}>
+          <div>
+            <span>Nome do evento</span>
+            <input type="text" />
+
+            <span>Cidade</span>
+            <input type="text" />
+
+            <span>UF</span>
+            <input type="text" />
+
+            <span>Data</span>
+            <input type="date" name="date" id="date" />
+            <button>Criar Evento</button>
+          </div>
+        </form>
+      </main>
+    </AppContainer>
   );
 };
 
