c8bfe7e3159db2c6714840a9b88789965c339916
Gustavo <gcrodrigues413@gmail.com>
2020-06-29 00:10:31 -0300

    Página de configuração do usuário

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { AppContainer } from "../../components";
 
 import styles from "./Settings.module.css";
 
 const Settings = () => {
   return (
-    <div>
-      <h1>Settings</h1>
-    </div>
+    <AppContainer>
+      <main className={styles.main}>
+        <h1>Settings</h1>
+      </main>
+    </AppContainer>
   );
 };
 
