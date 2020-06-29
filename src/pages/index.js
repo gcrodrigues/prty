f5e9c3b99368750a0de669cec726bb5830ed1e00
Gustavo <gcrodrigues413@gmail.com>
2020-06-29 00:14:38 -0300

    Arquivo index da /pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,7 @@ export { default as Signup } from "./Signup/Signup";
 export { default as Login } from "./Login/Login";
 export { default as Dashboard } from "./Dashboard/Dashboard";
 export { default as Events } from "./Events/Events";
+export { default as AllEvents } from "./AllEvents/AllEvents";
 export { default as Settings } from "./Settings/Settings";
+export { default as Search } from "./Search/Search";
+export { default as Favorites } from "./Favorites/Favorites";
