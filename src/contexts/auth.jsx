0c3906737be541736834e25b51fb44c9433b2618
Gustavo <gcrodrigues413@gmail.com>
2020-07-01 15:05:10 -0300

    Isolando funcionalidades de layout

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -5,8 +5,6 @@ const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [width, setWidth] = useState(window.innerWidth);
-  const [open, setOpen] = useState(true);
 
   async function signIn() {
     const response = await auth.SignIn();
@@ -15,18 +13,8 @@ export const AuthProvider = ({ children }) => {
     localStorage.setItem("user", response.user.name);
   }
 
-  function isOpen() {
-    setOpen(!open);
-  }
-
-  function resizeWidth() {
-    setWidth(window.innerWidth);
-  }
-
   return (
-    <AuthContext.Provider
-      value={{ signed: !!user, signIn, user, open, isOpen, resizeWidth, width }}
-    >
+    <AuthContext.Provider value={{ signed: !!user, signIn, user }}>
       {children}
     </AuthContext.Provider>
   );
