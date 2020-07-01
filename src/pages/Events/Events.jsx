01803af063651a2ae3c53be0345fa588a7d86f30
Gustavo <gcrodrigues413@gmail.com>
2020-07-01 14:55:58 -0300

    Overflow-y hidden se modalIsOpen

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AppContainer, EventList } from "../../components";
+import LayoutContext from "../../contexts/layout";
+import cn from "classnames";
 
 import styles from "./Events.module.css";
 
 const Events = () => {
+  const { modalIsOpen } = useContext(LayoutContext);
   return (
     <AppContainer>
-      <main className={styles.main}>
+      <main
+        className={
+          modalIsOpen ? cn(styles.main, styles.overflowHidden) : styles.main
+        }
+      >
         <h1>Seus Eventos</h1>
         <EventList />
       </main>
