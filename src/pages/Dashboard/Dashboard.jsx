37c2b6b5f00dabdc9197e0ac23b1395c99ec5a83
Gustavo <gcrodrigues413@gmail.com>
2020-07-01 15:07:31 -0300

    Implementação do método post de eventos

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "../../services/api";
+import { useHistory } from "react-router-dom";
 
 import { AppContainer } from "../../components";
 import styles from "./Dashboard.module.css";
 
 const Dashboard = () => {
+  const history = useHistory();
+  const [eventName, setEventName] = useState("");
+  const [local, setLocal] = useState("");
+  const [valor, setValor] = useState(0);
+  const [data, setData] = useState("");
+  const [descricao, setDescricao] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    await api.post("/eventos", {
+      nome: eventName,
+      local: local,
+      valor: valor,
+      descricao: descricao,
+      data: data,
+      usuario: null,
+    });
+
+    history.push("/myevents");
+  }
+
   return (
     <AppContainer>
       <main className={styles.main}>
         <h1>Cadastre seu evento</h1>
-        <form className={styles.form}>
-          <div>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.wrapper}>
             <span>Nome do evento</span>
-            <input type="text" />
+            <input type="text" onChange={(e) => setEventName(e.target.value)} />
 
-            <span>Cidade</span>
-            <input type="text" />
+            <span>Local</span>
+            <input type="text" onChange={(e) => setLocal(e.target.value)} />
 
-            <span>UF</span>
-            <input type="text" />
+            <span>Descrição</span>
+            <input type="text" onChange={(e) => setDescricao(e.target.value)} />
+            <div className={styles.dualInput}>
+              <div className={styles.inputWrapper}>
+                <span>Valor</span>
+                <input
+                  type="number"
+                  onChange={(e) => setValor(e.target.value)}
+                />
+              </div>
+              <div className={styles.inputWrapper}>
+                <span>Data</span>
+                <input
+                  type="date"
+                  name="date"
+                  id="date"
+                  onChange={(e) => setData(e.target.value)}
+                />
+              </div>
+            </div>
 
-            <span>Data</span>
-            <input type="date" name="date" id="date" />
             <button>Criar Evento</button>
           </div>
         </form>
